1717f941857290ca943d7bee832fbb77a8b05c4e
Donovan Rojas <rojasdrm03@gmail.com>
2023-08-10 14:45:02 -0500

    deepFreeze

--- main.js
+++ main.js
@@ -187,3 +187,31 @@ function deepCopy(subject) {
 
     return copySubject;
 }
+
+const juan = {
+    name: "Juanito",
+    approvedCourses: ["Curso 1","Curso 2"],
+    caracteristicas: {
+      age: 18,
+      colorCabello: 'Negro',
+      gustos: {
+        musica: ['rock', 'punk', 'ska'],
+        peliculas: ['drama', 'horros', 'comedia'],
+      },
+    },
+    addCourse(newCourse) {
+      console.log("This", this);
+      console.log("This.approvedCourses", this.approvedCourses);
+      this.approvedCourses.push(newCourse);
+    }
+  };
+
+  function deepFreeze(obj) {
+    if( typeof(obj)!=="object") return
+
+    Object.freeze(obj); 
+
+    for(let key in obj){
+      deepFreeze(obj[key]) 
+    }
+}
